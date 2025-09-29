57538d7ff7b4e03c030dfc24f2fb9b8b0a219380
PC-Ngumoha <pcngumoha@gmail.com>
2025-09-29 20:58:50 +0100

    Working with files & forms, handling errors.

---
+++ main_17.py
@@ -0,0 +1,21 @@
+"""
+Working with Files and Forms at the same time.
+"""
+from typing import Annotated
+
+from fastapi import FastAPI, Form, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post('/files/')
+async def create_file(
+    file: Annotated[bytes, File()],
+    fileb: Annotated[UploadFile, File()],
+    token: Annotated[str, Form()]
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
